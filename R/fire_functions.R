dae77f82844ef4ab2bfcfe91d43b648e19b711fb
Michael Bedward <michael.bedward@gmail.com>
2020-03-25 14:11:24 +1100

    tidy up functions for FFDI and friends

--- R/fire_functions.R
+++ R/fire_functions.R
@@ -1,34 +1,92 @@
 #' Calculate FFDI for specified weather stations
 #'
-#' @param db A database connection pool object as returned by
-#'   \code{\link{bom_db_open}} or \code{\link{bom_db_create}}.
+#' @param dat.stn A data frame with records for one or more weather stations.
+#'   Normally this will made up of records returned from querying the synoptic
+#'   or AWS tables in the database. If from another data source, the columns
+#'   and column names must conform to those used in the database tables for
+#'   variables used to calculate KBDI and FFDI values:
+#'   \code{'station', 'year', 'month', 'day', 'hour', 'minute',
+#'         'precipitation', 'temperature', 'relhumidity', 'windspeed'}
 #'
-#' @param stations A vector of one or more integer weather station identifiers.
+#' @param av.rainfall.years A vector of four digit year numbers for the
+#'   reference period over which average annual rainfall at each weather station
+#'   should be calculated. This is used in the KBDI calculation. Normally the
+#'   reference period will be a sequence of contiguous years, but it does not
+#'   have to be. Default is \code{2001:2015}.
+#'
+#' @param min.days.per.year The minimum number of days of rainfall data required
+#'   for each year specified by \code{av.rainfall.years}. Default is \code{300}.
+#'
+#' @return A data frame with columns:
+#'   \code{'station', 'year', 'month', 'day', 'hour', 'minute',}
+#'   \code{'tmax.daily'} (maximum temperature for calendar day),
+#'   \code{'precip.daily'} (total rainfall from 09:01 to 09:00 the next day),
+#'   \code{'drought'} (daily drought factor values),
+#'   \code{'ffdi'} (FFDI value based on daily drought factor and time-step
+#'   values for temperature, wind speed and relative humidity.
+#'
+#' @examples
+#' \dontrun{
+#' library(CERMBweather)
+#' library(dplyr, warn.conflicts = FALSE)
+#'
+#' # Get synoptic data for selected weather stations
+#' DB <- bom_db_open("/path/to/database/BOM.db")
+#'
+#' stns <- c(1006, 1007, 1019, 1020)
+#'
+#' dat <- bom_db_synoptic(DB) %>%
+#'   filter(station %in% stns) %>%
+#'   collect()
+#'
+#' # Calculate FFDI for stations
+#' ffdi <- bom_db_ffdi(dat)
+#'
+#' # If you want to relate FFDI and drought factor values to the
+#' # original sub-daily weather values, join to input data
+#' ffdi <- left_join(dat, ffdi,
+#'                   by = c("station", "year", "month", "day", "hour", "minute"))
+#'
+#' bom_db_close(DB)
+#' }
 #'
 #' @export
 #'
-calculate_station_ffdi <- function(db, stations,
-                                   av.rainfall.years = 2001:2015,
-                                   min.days.per.year = 300) {
+bom_db_ffdi <- function(dat,
+                        av.rainfall.years = 2001:2015,
+                        min.days.per.year = 300) {
 
-  res <- lapply(stations, function(stn) {
-    dat <- bom_db_synoptic(db) %>%
-      dplyr::filter(station == stn) %>%
-      dplyr::collect()
+  colnames(dat) <- tolower(colnames(dat))
 
-    if (nrow(dat) > 0) {
-      .do_station_ffdi(dat, av.rainfall.years, min.days.per.year)
-    } else {
-      warning("No records for station ", stn, immediate. = TRUE)
-      NULL
-    }
+  RequiredCols <- c("station", "year", "month", "day", "hour", "minute",
+                    "precipitation", "temperature", "relhumidity", "windspeed")
+
+  found <- RequiredCols %in% colnames(dat)
+  if (any(!found)) {
+    stop("The following required columns are not present:\n",
+         RequiredCols[!found])
+  }
+
+  stations <- unique(dat$station)
+
+  res <- lapply(stations, function(the.stn) {
+    .do_calculate_ffdi(dat %>% dplyr::filter(station == the.stn),
+                       av.rainfall.years,
+                       min.days.per.year)
   })
 
   dplyr::bind_rows(res)
 }
 
 
-.do_station_ffdi <- function(dat.stn, av.rainfall.years, min.days.per.year) {
+# Private worker function for bom_db_ffdi
+#
+.do_calculate_ffdi <- function(dat.stn, av.rainfall.years, min.days.per.year) {
+
+  # Value to use when drought factor is zero so that the log(DF) term
+  # in FFDI calculation does not result in missing values
+  MinDroughtFactor = 1e-3
+
   dat.stn <- dat.stn %>%
     # Ensure only one record per time point (this will arbitrarily
     # take the first from any duplicate set)
@@ -81,29 +139,34 @@ calculate_station_ffdi <- function(db, stations,
 
     dplyr::ungroup() %>%
 
-    dplyr::mutate(kbdi = calculate_daily_kbdi(precip.daily, tmax.daily, AvRainfall) )
+    dplyr::bind_cols(bom_db_drought(.$precip.daily, .$tmax.daily, AvRainfall))
 
 
   dat.stn <- dat.stn %>%
     # Join daily value of KBDI to original time-step data
     dplyr::left_join(dat.daily, by = c("year", "month", "day")) %>%
 
+    dplyr::mutate(drought = ifelse(drought <= 0, MinDroughtFactor, drought)) %>%
+
     # Add calculated FFDI values and return the data frame.
     # Missing values in any of the input variables will propagate
     # through to FFDI auto-magically.
     dplyr::mutate(ffdi = round(
-      2 * exp(0.987 * log(kbdi) - 0.45 +
+      2 * exp(0.987 * log(drought) - 0.45 +
                 0.0338 * temperature +
                 0.0234 * windspeed -
                 0.0345 * relhumidity),
       digits = 1))
 
-  dat.stn
+  dat.stn %>%
+    dplyr::select(station, year, month, day, hour, minute,
+                  tmax.daily, precip.daily, kbdi, drought, ffdi)
 }
 
 
-#' Calculate the Keetch-Byram drought index (soil moisture deficit)
+#' Calculate daily drought factor
 #'
+#' Calculates daily drought factor based on the Keetch-Byram drought index
 #' Following Finkele et al. (2006), KBDI is calculated as yesterday's KBDI
 #' plus evapo-transpiration minus effective rainfall.
 #'
@@ -115,14 +178,13 @@ calculate_station_ffdi <- function(db, stations,
 #'
 #' @param average.rainfall Reference value for average annual rainfall.
 #'
-#' @return A vector of KBDI values of the same length as the input precipitation
-#'   and temperature vectors.
+#' @return A data frame with columns kbdi and drought.
 #'
 #' @export
 #'
-calculate_daily_kbdi <- function(daily.precip,
-                                 daily.tmax,
-                                 average.rainfall) {
+bom_db_drought <- function(daily.precip,
+                           daily.tmax,
+                           average.rainfall) {
 
   if (length(daily.precip) != length(daily.tmax)) {
     stop("Input vectors for precipitation and maximum temperature differ in length")
@@ -132,110 +194,128 @@ calculate_daily_kbdi <- function(daily.precip,
   Kday <- numeric(Ndays)
   ET <- numeric(Ndays)
 
-  daily.precip.eff <- calculate_effective_rainfall(daily.precip)
+  daily.precip.eff <- bom_db_effective_rainfall(daily.precip)
 
-  # Term 3, annual rainfall influence
-  ET3 <- 1+10.88*exp(-.001736 * average.rainfall)
+  # Term 3 in ET equation, annual rainfall influence
+  ET3 <- 1 + 10.88 * exp(-.001736 * average.rainfall)
 
   # Term 4 in ET equation
-  ET4 <- 1.e-3
+  ET4 <- 1e-3
 
   # First step requires two days of records.
   # Set to maximum dryness value
   Kday[1:2] <- 203.2
 
-  # Helper for loop below
-  clamp <- function(x) min(203.2, max(0, round(x, digits = 1)))
+  # Helper for KBDI loop below
+  clampK <- function(x) min(203.2, max(0, round(x, digits = 1)))
 
   for (i in 3:Ndays) {
-    # ET applies yesterday's max temp to yesterday's KDBI, adjusted
-    # for mean annual rainfall, to increase the soil moisture deficit
-    ET1 <- 203.2 - Kday[i-1]
+    Kprev <- Kday[i-1]
+
+    if (is.na(Kprev)) {
+      # [Hamish] Missing Tmax or precip.eff values result in all subsequent
+      # ET and KBDI values being set to missing because of the lagged effect
+      # of these variables. We need to specify when it is OK to ignore previous
+      # missing values.
+      #
+      # In the approach used here, if Tmax and rain values are present
+      # for days i and i-1, then we use the most recent non-missing
+      # KBDI value from not more than 30 days previously. If no previous
+      # data are available for that period, we set previous KBDI to
+      # maximum deficit of 203.2
+      #
+      Krecent <- na.omit( Kday[(i-2):max(1, i-30)] )
+      if (length(Krecent) > 0) {
+        # most recent non-missing value
+        Kprev <- Krecent[1]
+      } else {
+        # no values available - set to max deficit
+        Kprev <- 203.2
+      }
+    }
+
+    ET1 <- 203.2 - Kprev
     ET2 <- 0.968 * exp(0.0875 * daily.tmax[i-1] + 1.5552) - 8.30
     ET[i] <- ((ET1 * ET2) / ET3) * ET4
 
-    if (is.na(daily.tmax[i]) || is.na(daily.precip[i])) {
-      Kday[i] <- NA
-    } else {
-      Kday[i] <- clamp(Kday[i-1] - daily.precip.eff[i] + ET[i])
-    }
+    Kday[i] <- clampK( Kprev - daily.precip.eff[i] + ET[i] )
   }
 
-  # drought factor rain. Nonsense about last 20 days, etc.
-  # An event can be multi-dayed, if over 2 mm each day.
-  # In that case, the timing of the event is the day with the most rain.
+
+  # Calculate drought factor values.
+  # DF is based on the following over the last 20 days:
+  #   E - rainfall event, defined as consec days each with rain > 2mm
+  #   P - past rainfall amount i.e. sum over event E
+  #   N - days since day of largest rainfall within event
   DFday <- numeric(Ndays)
   DFday[1:20] <- NA
 
-  # start on day 21 because need 20 days before,
+  # For drought factor, only rainfall of 2mm or more
+  # is considered
+  prDF <- daily.precip
+  prDF[ daily.precip <= 2 ] <- 0
+
   for (i in 21:Ndays) {
-    iiprev <- (i-20):(i-1)
-    rain.window <- daily.precip.eff[iiprev]
-    kb <- Kday[iiprev]
+    if (is.na(Kday[i]) || is.na(daily.precip[i])) {
+      DFday[i] <- NA
 
-    curr.rain <- 0		# amount / P
-    curr.x <- -999
-    curr.age <- 0
-    sig.rain <- 0
-    sig.x <- -999
-    sig.age <- 0
-    lim.x <- 1
-    most.rain <- 0
+    } else {
+      # Find most recent rainfall event not more than 20 days ago
+      ii <- (i-1):(i-20)
 
-    for (j in 20:1) {
-      # rain must exceed 2mm to qualify
-      if (is.na(rain.window[j]) || rain.window[j] < 2) {
-        most.rain <- 0
-        curr.rain <- 0
-        curr.age <- 20-j
-        curr.x <- 1
+      r20 <- prDF[ii]
+      r20.flag <- prDF[ii] > 0
+
+      # Look for instances of two consecutive days with rain
+      is.event <- r20.flag & lead(r20.flag)
+
+      # last is.event element will be NA, change to FALSE
+      is.event[length(r20.flag)] <- FALSE
+
+      if (!any(is.event)) {
+        # No runs of two or more rainy days
+        xraw <- 1
+
       } else {
-        curr.rain <- curr.rain + rain.window[j]
-        if (rain.window[j] >= most.rain) {
-          curr.age <- 20 - j
-          most.rain <- rain.window[j]
-        }
+        # start index of most recent event
+        ev.start <- which(is.event)[1]
+
+        # end index of most recent event
+        ii <- which(!r20.flag)
+        ii <- ii[ii > ev.start]
+        ev.end <- ifelse(length(ii) == 0, length(r20.flag), ii[1] - 1)
+
+        # event rainfall
+        E <- r20[ev.start:ev.end]
+        P <- sum(E)
+
+        # index of maximum rainfall in the window
+        # (days since most rain)
+        Emax.i <- which.max(E) + ev.start - 1
+
+        # Following Finkele et al. (2006)
+        Emax.term <- ifelse(Emax.i == 1, 0.8, Emax.i - 1)
+        xraw <- (Emax.term^1.3) / (Emax.term^1.3 + P - 2)
       }
 
-      if (curr.rain > 2) {
-        if (curr.age == 0) curr.age <- 0.8
-        curr.x <- curr.age^1.3 / (curr.age^1.3 + curr.rain - 2)
+      if (Kday[i] >= 20) {
+        xlim <- 75 / (270.525 - 1.267 * Kday[i])
       } else {
-        curr.x <- 1
+        xlim <- x
       }
 
-      if (sig.rain > 2) {
-        if (sig.age == 0) sig.age <- 0.8
-        sig.x <- sig.age^1.3 / (sig.age^1.3 + sig.rain - 2)
-      } else {
-        sig.x <- 1
-      }
+      x <- min(xraw, xlim)
+      xfactor <- (41 * x^2 + x) / (40 * x^2 + x + 1)
 
-      if (curr.x <= sig.x) {
-        sig.rain <- curr.rain
-        sig.x <- curr.x
-        sig.age <- curr.age
-      }
+      DFbase <- 10.5* (1.0 - exp(-(Kday[i] + 30) / 40))
+      DFraw <- DFbase * xfactor
 
-      if (kb[j] < 20) {
-        lim.x <- 1 / (1 + 0.1135 * kb[j])
-      } else {
-        lim.x <- 75 / (270.525 - 1.267 * kb[j])
-      }
-
-      if (lim.x < sig.x) sig.x <- lim.x
+      # If DFraw is NA, allow this to propagate to DFday
+      DFday[i] <- round( min(10, DFraw), digits = 1)
     }
-
-    factor1 <- (41 * sig.x^2 + sig.x) / (40 * sig.x^2 + sig.x + 1)
-
-    Kyest <- Kday[i-1]
-    DF.base <- 10.5 * (1 - exp(-(Kyest + 30) / 40) )
-    DF.final1 <- DF.base * factor1
-
-    DFday[i] <- round(min(10, DF.final1), digits = 1)
   }
 
-  DFday
+  data.frame(kbdi = Kday, drought = DFday)
 }
 
 
@@ -253,7 +333,7 @@ calculate_daily_kbdi <- function(daily.precip,
 #'
 #' @export
 #'
-calculate_effective_rainfall <- function(precip.daily) {
+bom_db_effective_rainfall <- function(precip.daily) {
   Ndays <- length(precip.daily)
   peff <- numeric(Ndays)
   bal <- 0
