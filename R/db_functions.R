ae46fa82c85688132c89630a7e4aa392284d7e9b
Michael Bedward <michael.bedward@gmail.com>
2021-10-05 13:29:49 +1100

    function to drive FFDI calculations

--- R/db_functions.R
+++ R/db_functions.R
@@ -9,7 +9,6 @@
 #' extension '.txt'. Any records that are duplicate those already in the
 #' database are silently ignored. Note that fire-related variables (KBDI,
 #' drought factor and FFDI) are \strong{not} calculated for the new records.
-#' \strong{TODO: link to the CERMBffdi package}
 #'
 #' @param db A database connection object created with \code{pool::dbPool} or
 #'   \code{DBI::dbConnect}.
@@ -35,7 +34,7 @@
 #' @examples
 #' \dontrun{
 #' # Connect to a PostgreSQL database using an admin user name and password
-#' DB <- DBI::dbConnect(drv = RPostgres::Postgres(),
+#' db <- DBI::dbConnect(drv = RPostgres::Postgres(),
 #'                      host = "some.host.uow.edu.au",
 #'                      dbname = "cermb_weather",
 #'                      user = "admin_user_name",
@@ -43,16 +42,16 @@
 #'
 #' # Import data from a BOM zip file containing individuals CSV-format
 #' # data files for weather stations
-#' bom_db_import(DB, "c:/bom/updates/some_aws.zip")
+#' bom_db_import(db, "c:/bom/updates/some_aws.zip")
 #'
 #' # Import data from CSV-format files in a directory
-#' bom_db_import(DB, "c:/bom/archive/aws_files/")
+#' bom_db_import(db, "c:/bom/archive/aws_files/")
 #'
 #' # Import data from an individual CSV-format weather station file
-#' bom_db_import(DB, "c:/foo/bar/HC06D_Data_068228_999999999515426.txt")
+#' bom_db_import(db, "c:/foo/bar/HC06D_Data_068228_999999999515426.txt")
 #'
 #' # Do other things, then at end of session...
-#' DBI::dbDisconnect(DB)
+#' DBI::dbDisconnect(db)
 #' }
 #'
 #' @export
@@ -221,6 +220,305 @@ bom_db_import <- function(db,
     TRUE
   }
 }
+
+
+#' Add values for KBDI, Drought Factor and FFDI to database records
+#'
+#' This function uses the \code{CERMBffdi} package to calculate values for the
+#' fire related variables: KBDI, drought factor and FFDI for selected weather
+#' records, and store the values in the corresponding database columns. With
+#' default arguments, the function will process all weather stations in the
+#' 'aws' and 'synoptic' tables that were active on or after 2000-01-01. The
+#' function attempts to minimize data transfer over the network and running time
+#' by only processing the most recent records that do not already have FFDI
+#' values. Set the argument \code{recalculate=TRUE} to override this behaviour
+#' and force recalculation for all records.
+#'
+#' @param db A database connection object created with \code{pool::dbPool} or
+#'   \code{DBI::dbConnect}.
+#'
+#' @param tables The tables to process. One or both of \code{'aws'} and \code{'synoptic'}.
+#'   Default is both.
+#'
+#' @param states Standard abbreviations for the states to process. Default is
+#'   \code{c('NSW', 'VIC', 'ACT', 'TAS', 'QLD', 'SA', 'WA')}.
+#'
+#' @param cutoff Cutoff date to subset stations to process. Values will only be
+#'   calculated for stations that were active (i.e. have records in the
+#'   database) on or after this date. Can be specified as a \code{Date} object
+#'   or a character string in \code{'yyyy-mm-dd'} format. Default is
+#'   \code{'2000-01-01'}.
+#'
+#' @param recalculate If \code{TRUE}, values will be calculated for all records and any existing values
+#'   will be over-written. If \code{FALSE} (default), values will only be calculated for the most recent
+#'   records that do not already have FFDI values.
+#'
+bom_db_add_ffdi <- function(db,
+                            tables = c("synoptic", "aws"),
+                            states = c("NSW", "VIC", "ACT", "TAS", "QLD", "SA", "WA"),
+                            cutoff = as.Date("2000-01-01"),
+                            recalculate = FALSE) {
+
+  tables <- match.arg(tolower(tables),
+                      choices = c("synoptic", "aws"),
+                      several.ok = TRUE)
+
+  states <- match.arg(toupper(states),
+                      choices = c("NSW", "VIC", "ACT", "TAS", "QLD", "SA", "WA"),
+                      several.ok = TRUE)
+
+  if (!length(cutoff == 1)) {
+    stop("cutoff should be a single Date object or character string in date format")
+  }
+
+  x <- inherits(cutoff, c("Date", "character"), which = TRUE)
+  if (sum(x) == 0) {
+    stop("cutoff should be a Date or a character string in date format")
+  } else if (x[2] == 1) {
+    cutoff <- as.Date(cutoff)
+  }
+
+
+  # Check that the database connection is valid
+  ok <- CERMBweather::bom_db_is_connected(db)
+  if (!ok) stop("Bummer - something wrong with database connection")
+
+
+  # Helper function to recode missing quality flag values as 'X'
+  quality_na2x <- function(x) {
+    x <- toupper(x)
+    ifelse(grepl("[YNWSI]", x), x, "X")
+  }
+
+
+  for (the.table in tables) {
+
+    # Query the stations in the specified table and state(s) that
+    # have data for the cutoff date and/or later.
+    #
+    for (the.state in states) {
+      msg <- glue::glue("Processing {the.state} {the.table} stations")
+      cat(msg, "\n")
+
+      cmd <- glue::glue(
+        "SELECT s.station, s.annualprecip_narclim, sub.most_recent
+       FROM stations s
+       INNER JOIN LATERAL (
+         SELECT station, date_local AS most_recent
+         FROM {the.table} t
+         WHERE t.station = s.station
+         ORDER BY date_local DESC
+         LIMIT 1) sub
+       ON TRUE
+
+       WHERE s.state = '{the.state}' AND
+         most_recent IS NOT NULL AND
+         annualprecip_narclim IS NOT NULL AND
+         most_recent >= '{cutoff}'::date
+       ORDER BY s.station;")
+
+      selected.stations <- DBI::dbGetQuery(db, cmd)
+
+      if (nrow(selected.stations) > 0) {
+        # --- Process each of the stations ---
+        #
+        pb <- progress::progress_bar$new(total = nrow(selected.stations),
+                                         format = "[:bar] :current of :total - :percent :eta")
+
+        for (istn in seq_len(nrow(selected.stations))) {
+          the.station <- selected.stations$station[istn]
+          the.avrain <- selected.stations$annualprecip_narclim[istn]
+          the.latest_date <- selected.stations$most_recent[istn]
+
+          pb$tick()
+
+          # Find the date range for existing FFDI values for this station
+          cmd <- glue::glue("SELECT station, first_ffdi_date, last_ffdi_date
+                           FROM {the.table}_ffdi_dates
+                           WHERE station = {the.station};")
+
+          dat_existing_dates <- DBI::dbGetQuery(db, cmd)
+
+          # Check whether we need to do this station
+          if (recalculate ||
+              nrow(dat_existing_dates) == 0 ||
+              dat_existing_dates$last_ffdi_date < the.latest_date) {
+
+            # Get station data. If not recalculating for all records,
+            # just get the data for last 90 days to avoid wasting time
+            # transferring a large data set over the network, but still
+            # have enough wiggle room for KBDI etc.
+            #
+            if (recalculate) {
+              date0 <- as.Date("1800-01-01")
+
+            } else {
+              date0 <- as.Date(the.latest_date) - 90
+            }
+
+            cmd <- glue::glue("SELECT * FROM {the.table}
+                             WHERE station = {the.station} AND
+                               date_local >= '{date0}'::date
+                             ORDER BY date_local, hour_local, min_local;")
+
+            dat <- DBI::dbGetQuery(db, cmd)
+            Ndata_recs <- nrow(dat)
+
+            if (Ndata_recs == 0) {
+              # This should never happen because the query for selected stations
+              # is based on the data table - so if we are here something
+              # has gone bady wrong!
+              msg <- glue::glue("Bummer - no data for {the.table} station {the.station}
+                               even though there should be")
+              stop(msg)
+            }
+
+            the.earliest_date <- dat$date_local[1]
+
+            # Check that the time series is long enough
+            ndays = max(dat$date_local) - min(dat$date_local)
+            if (ndays > 21) {
+              res <- calculate_ffdi(dat,
+                                    av.rainfall = the.avrain,
+                                    datatype = the.table,
+                                    for.records = "all")
+
+              res <- res %>%
+                dplyr::select(station,
+                              date_local = date, hour_local = hour, min_local = minute,
+                              tmaxdaily, precipdaily, kbdi, drought, ffdi)
+
+              # Set FFDI quality based on the lowest quality class of
+              # input variables. Quality classes (highest to lowest) are
+              #
+              #  Y: quality controlled and acceptable
+              #  N: not quality controlled
+              #  X: one or more input variables with missing quality code
+              #  I: quality controlled and inconsistent with other known information
+              #  S: quality controlled and considered suspect
+              #  W: quality controlled and considered wrong
+              #
+              q_in <- dat %>%
+                dplyr::select(precipitation_quality,
+                              relhumidity_quality,
+                              temperature_quality,
+                              windspeed_quality) %>%
+
+                dplyr::mutate(dplyr::across(dplyr::everything(), quality_na2x)) %>%
+
+                as.matrix()
+
+
+              res$ffdi_quality <- apply(q_in, 1, function(qrow) {
+                intersect(c("W", "S", "I", "X", "N", "Y"), qrow)[1]
+              })
+
+              ii <- is.na(res$ffdi)
+              res$ffdi_quality[ii] <- NA
+
+
+              # Unless recalculate is TRUE, only transfer
+              # the newly calculated records. Just to be safe, don't
+              # assume that `dat` and `res` have the same number and order
+              # of records, even though they should.
+              #
+              if (!recalculate) {
+                res <- dat %>%
+                  dplyr::transmute(date_local, hour_local, min_local, had_ffdi = !is.na(ffdi)) %>%
+
+                  dplyr::left_join(res, by = c("date_local", "hour_local", "min_local")) %>%
+
+                  # Retain records that have FFDI in `res` but did not in `dat`
+                  dplyr::filter(!is.na(ffdi), !had_ffdi) %>%
+
+                  dplyr::select(-had_ffdi)
+              }
+
+              if (nrow(res) > 0) {
+                temptbl.name <- paste0(the.table, "_ffdi_temp")
+
+                sql.create_temp_table <- glue::glue("
+                CREATE TEMPORARY TABLE {temptbl.name}
+                ON COMMIT DROP
+                AS
+                SELECT station, date_local, hour_local, min_local,
+                tmaxdaily, precipdaily, kbdi, drought, ffdi, ffdi_quality
+                FROM {the.table}
+                WITH NO DATA;")
+
+                sql.index_temp_table <- glue::glue("
+                CREATE INDEX idx_tmp_date_local
+                ON {temptbl.name} USING btree
+                (date_local ASC);")
+
+                sql.analyze_temp_table <- glue::glue("
+                ANALYZE {temptbl.name};")
+
+                sql.update_recs <- glue::glue("
+                UPDATE {the.table} m
+                SET tmaxdaily = t.tmaxdaily,
+                precipdaily = t.precipdaily,
+                kbdi = t.kbdi,
+                drought = t.drought,
+                ffdi = t.ffdi,
+                ffdi_quality = t.ffdi_quality
+                FROM {temptbl.name} t
+                WHERE m.station = t.station
+                AND m.date_local = t.date_local
+                AND m.hour_local = t.hour_local
+                AND m.min_local = t.min_local;")
+
+                sql.drop_temp_table <- glue::glue("
+                DROP TABLE {temptbl.name};")
+
+                if (.is_pool_connection(db)) {
+                  pool::poolWithTransaction(db, function(conn) {
+                    DBI::dbExecute(conn, sql.create_temp_table)
+                    DBI::dbWriteTable(conn, temptbl.name, res, overwrite = TRUE)
+
+                    DBI::dbExecute(conn, sql.index_temp_table)
+                    DBI::dbExecute(conn, sql.analyze_temp_table)
+
+                    DBI::dbExecute(conn, sql.update_recs)
+
+                    DBI::dbExecute(conn, sql.drop_temp_table)
+                  })
+                } else if (.is_dbi_connection(db)) {
+                  DBI::dbWithTransaction(db, {
+                    DBI::dbExecute(db, sql.create_temp_table)
+                    DBI::dbWriteTable(db, temptbl.name, res, overwrite = TRUE)
+
+                    DBI::dbExecute(conn, sql.index_temp_table)
+                    DBI::dbExecute(conn, sql.analyze_temp_table)
+
+                    DBI::dbExecute(conn, sql.update_recs)
+
+                    DBI::dbExecute(conn, sql.drop_temp_table)
+                  })
+                }
+
+              } # end of writing records to db
+
+            } # end of check for ndays > 21
+
+          } # end of check for FFDI calculation required
+
+        } # end of station loop
+
+      } # end of check for 1 or more selected stations
+
+    } # end of state loop
+
+
+    # Refresh the materialized view for FFDI dates
+    message(glue::glue("Refreshing materialized view {the.table}_ffdi_dates"))
+
+    cmd <- glue::glue("REFRESH MATERIALIZED VIEW CONCURRENTLY {the.table}_ffdi_dates;")
+    DBI::dbExecute(db, cmd)
+  }
+
+}
+
 
 
 #' Get count of records.
